bdba360194dfd2821b290ebd4c90a3150d435e60
Emi <emilijaopsenica@gmail.com>
2026-06-13 01:35:04 +0200

    Figure out how Lombok works

--- app/src/main/java/com/example/slagalica/domain/model/match/games/KoZnaZna.java
+++ app/src/main/java/com/example/slagalica/domain/model/match/games/KoZnaZna.java
@@ -3,51 +3,21 @@ package com.example.slagalica.domain.model.match.games;
 import com.google.firebase.firestore.DocumentId;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class KoZnaZna {
     @DocumentId
     private String koZnaZnaId;
     private String question;
     private String correctAnswer;
     private List<String> otherAnswers;
-
-    public KoZnaZna() {}
-
-    public KoZnaZna(String koZnaZnaId, String question, String correctAnswer, List<String> otherAnswers) {
-        this.koZnaZnaId = koZnaZnaId;
-        this.question = question;
-        this.correctAnswer = correctAnswer;
-        this.otherAnswers = otherAnswers;
-    }
-
-    public String getKoZnaZnaId() {
-        return koZnaZnaId;
-    }
-
-    public void setKoZnaZnaId(String koZnaZnaId) {
-        this.koZnaZnaId = koZnaZnaId;
-    }
-
-    public String getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    public String getCorrectAnswer() {
-        return correctAnswer;
-    }
-
-    public void setCorrectAnswer(String correctAnswer) {
-        this.correctAnswer = correctAnswer;
-    }
-
-    public List<String> getOtherAnswers() {
-        return otherAnswers;
-    }
-
-    public void setOtherAnswers(List<String> otherAnswers) {
-        this.otherAnswers = otherAnswers;
-    }
 }
